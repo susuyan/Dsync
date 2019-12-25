6366c004398576f0951197f3ed752e1a34f71354
susuyan <susuyan@163.com>
2019-12-25 10:24:30 +0800

    initial

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dsync/dsync.dart' as dsync;
+
+void main(List<String> arguments) async {
+  dsync.hello();
+}
---
+++ lib/dsync.dart
@@ -0,0 +1,9 @@
+import 'dart:ffi';
+
+import 'package:cli_util/cli_logging.dart';
+
+Void hello() {
+  Logger logger = Logger.standard();
+
+  logger.stdout('hello world');
+}
